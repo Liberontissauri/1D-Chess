c563c7af3f739fc85997dc06ed40eebb30e58bdc
Liberontissauri <jp.liberon@gmail.com>
2021-02-15 11:49:47 +0000

    Added the Rook

--- index.js
+++ index.js
@@ -51,7 +51,10 @@ class Board1D {
     addPiece(location, piece_name = "pawn", team) {
         switch (piece_name){
             case "pawn":
-                this.boardSquares[location].piece = new Pawn(this.boardSquares[location], team)
+                this.boardSquares[location].piece = new Pawn(this.boardSquares[location], team);
+                break;
+            case "rook":
+                this.boardSquares[location].piece = new Rook(this.boardSquares[location], team);
         }
         this.boardSquares[location].updateImg()
     }
@@ -127,7 +130,6 @@ class Piece {
 
     move(location) {
         if(this.canMove(location)) {
-
             this.square.piece = null;
             this.square.updateImg()
             this.square = this.square.board.boardSquares[location]
@@ -166,8 +168,52 @@ class Pawn extends Piece {
     }
 }
 
+class Rook extends Piece {
+    constructor(square, team) {
+        super(square, team);
+        this.name = "rook";
+        this.pieceImg = this.setupImg();
+    }
 
+    canMove(location) {
+        if (this.square.location < location) {
+            
+            for (let i = this.square.location + 1 ; i < location; i++) {
+                if (this.square.board.boardSquares[i].piece != null) {
+
+                    return false;
+                }
+            }
+            return true;
+
+        } else if (this.square.location > location) {
+
+            for (let i = this.square.location - 1 ; i > location; i--) {
+                if (this.square.board.boardSquares[i].piece != null) {
+                    
+                    return false;
+                }
+            }
+            return true;
+            
+        }
+    }
+
+    setupImg() {
+        let img = document.createElement("img");
+        img.src = "./img/pieces/chess-rook.png";
+        img.classList.add("iconPiece");
+        if (this.team == "black") {
+            img.classList.add("blackPiece");
+        } else {
+            img.classList.add("whitePiece");
+        }
+
+        return img;
+    }
+}
 
 let Game = new Board1D(BoardDiv);
 
+Game.addPiece(8, "rook")
 Game.addPiece(1, "pawn")
